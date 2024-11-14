f22e594c28ab20fcbeda24be92f6a2129700c0e8
Jeff Martin <chess.logic@gmail.com>
2024-11-14 14:23:38 -0800

    Remove probematic call to getenv()

--- home.py
+++ home.py
@@ -2,7 +2,6 @@
 
 import streamlit as st
 
-
 st.header(":orange[:material/public:] Home", divider="orange")
 
 st.sidebar.subheader(":orange[:material/public:] Home")
--- pages/lunar.py
+++ pages/lunar.py
@@ -4,8 +4,8 @@ import streamlit as st
 import base64
 import os
 
-userpass = os.getenv()  # "applicationId:applicationSecret"
-authString = base64.b64encode(userpass.encode()).decode()
+# userpass = os.getenv()  # "applicationId:applicationSecret"
+# authString = base64.b64encode(userpass.encode()).decode()
 
 st.header(":orange[:material/bedtime:] Lunar Phase")
 st.sidebar.subheader(":orange[:material/bedtime:] Lunar Phase")
